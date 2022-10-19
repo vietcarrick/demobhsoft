a0bf713ea0634afbe95c6375249e874b51d188cb
Ngo Quoc Viet <vietdun1@gmail.com>
2022-10-19 13:17:31 +0700

    responsive mobile

---
+++ index.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.review-web').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+      });
+});
+$(document).ready(function(){
+    $('.review-mobile').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+      });
+});
